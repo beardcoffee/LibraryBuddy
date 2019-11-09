fb62db3c3a72d251821766e2fd5c08c8e495f42e
Brian Beard <bjbeard@uno.edu>
2019-11-09 00:06:13 -0600

    Added basic layout for the remaining fragments, subject to change

--- app/src/main/java/com/example/librarybuddy/FragmentThree.java
+++ app/src/main/java/com/example/librarybuddy/FragmentThree.java
@@ -8,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 
-
-public class FragmentThree extends Fragment {
+public class FragmentThree extends Fragment implements AdapterView.OnItemSelectedListener {
 
     public FragmentThree() {
         // Required empty public constructor
@@ -24,14 +28,33 @@ public class FragmentThree extends Fragment {
 
     }
 
+    private ArrayList<String> dates = new ArrayList<String>(){{
+      add("October 28");
+      add("October 29");
+    }};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_three, container, false);
+        Spinner spinner = (Spinner) v.findViewById(R.id.spinnerDates);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(),
+                android.R.layout.simple_spinner_item,
+                dates);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
         return v;
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
